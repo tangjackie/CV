fad1d9469feae7344f1f5a74b3ba971ddf4b9826
tangjackie <tangj26@gmail.com>
2020-09-21 01:08:50 -0400

    Customize with my info

--- setup_cv.R
+++ setup_cv.R
@@ -10,7 +10,7 @@ devtools::install_github("nstrayer/datadrivencv")
 ?datadrivencv::use_datadriven_cv()
 datadrivencv::use_datadriven_cv(
   full_name = "Jackie Tang",
-  data_location = "https://docs.google.com/spreadsheets/d/1UksSMngO_n0SNxbgIJKlTk2sOtBLrpb5HmXl9GxBQv0/edit#gid=917338460",
+  data_location = "https://docs.google.com/spreadsheets/d/1UksSMngO_n0SNxbgIJKlTk2sOtBLrpb5HmXl9GxBQv0/edit?usp=sharing",
   pdf_location = "https://github.com/tangjackie/CV/raw/master/jackietang_cv.pdf",
   html_location = "tangjackie.github.io",
   source_location = "https://github.com/tangjackie/CV"
