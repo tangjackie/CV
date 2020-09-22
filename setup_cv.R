577f1fef55ae62bcf2f7cee69bbe230b6a6f8950
tangjackie <tangj26@gmail.com>
2020-09-22 06:12:07 -0400

    add catch data option

--- setup_cv.R
+++ setup_cv.R
@@ -17,5 +17,19 @@ datadrivencv::use_datadriven_cv(
 )
 
 servr::httd()
-To stop the server, run servr::daemon_stop(1) or restart your R session
-Serving the directory C:\Users\tangj\Desktop\R\CV at http://127.0.0.1:4321
+#To stop the server, run servr::daemon_stop(1) or restart your R session
+#Serving the directory C:\Users\tangj\Desktop\R\CV at http://127.0.0.1:4321
+
+pagedown::find_chrome()
+servr::daemon_stop(1)
+library(httpuv)
+library(tidyr)
+
+export_cv <- function(){pagedown::chrome_print("index.html")}
+export_cv()
+  
+  
+  
+  
+  
+  
