b64cddd66e24c2956774ec7df6af7f9ab9685d96
tangjackie <tangj26@gmail.com>
2020-09-22 00:57:13 -0400

    create a pdf version

--- setup_cv.R
+++ setup_cv.R
@@ -15,3 +15,7 @@ datadrivencv::use_datadriven_cv(
   html_location = "tangjackie.github.io",
   source_location = "https://github.com/tangjackie/CV"
 )
+
+servr::httd()
+To stop the server, run servr::daemon_stop(1) or restart your R session
+Serving the directory C:\Users\tangj\Desktop\R\CV at http://127.0.0.1:4321
